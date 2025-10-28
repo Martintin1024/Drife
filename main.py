d737ebdecdee0d66e9825e15d2a25414aee5a6c2
Martintin1024 <thefraok3010@proton.me>
2025-10-28 11:31:13 -0300

    agrego la base de datos
    tambien pude crear un usuario

--- main.py
+++ main.py
@@ -1,157 +1,33 @@
-import random
+import sqlite3
+import pandas as pd
 
-### DICCIONARIO PARA RANDOMIZADO ###
+## CREAR UN USUARIO ##
 
+try:
+    conn = sqlite3.connect("baseDeLaRuleta.db")
 
-## DICCIONARIO CHILL ##
+    cursor = conn.cursor()
 
-opciones_chill = {
-    1 : 0.25,
-    2 : 0.20,
-    3 : 0.05,
-    4 : 0.05,
-    5 : 0.20,
-    6 : 0.20,
-    7 : 0.05
-}
+    nuevoUsuario = "Martin"
+    sqlInsertar = "INSERT INTO Usuario (nombreUsuario) VALUES (?)"
 
-## DICCIONARIO CHILL ##
+    cursor.execute(sqlInsertar, (nuevoUsuario,))
 
+    conn.commit()
 
-## DICCIONARIO TOCHO ##
+    print(f"¡El usuario ", nuevoUsuario, " fue agregado con éxito!")
 
-opciones_tocho = {
-    1 : 0.30,
-    2 : 0.30,
-    3 : 0.30,
-    4 : 0.10
-}
+except sqlite3.Error as e:
+    print(f"Error al insertar datos en la base de datos: ", e)
+    if conn:
+        conn.rollback()
 
-## DICCIONARIO TOCHO ##
+finally:
+    if conn:
+        conn.close()
+        print("Saliendo de la base de datos")
 
+print("-" * 20)
 
+## VER USUARIOS ##
 
-### FUNCIONES ###
-
-## RECOMPENSAS CHILL ##
-
-def nada():
-    print("Respirá profundo y seguí con tu vida crack")
-
-def meditar():
-    print("Frenate 10 minutos y meditá, que te vas a sentir mucho más tranquilo")
-
-def musica():
-    print("Escuchate dos temitas pa y volvé a la carga")
-
-def comer():
-    print("Hacete algo para morfar gordito, que seguro quemaste bastante energía")
-
-def mona():
-    print("Andá a jugar con mona, le vas a hacer un favor a los dos")
-
-def sol():
-    print("La vitamina D te llama, tomate un ratito de sol")
-
-def baño():
-    print("Bañate sucio (mentira, te quiero)")
-
-## RECOMPENSAS CHILL ##
-
-
-
-## RECOMPENSAS TOCHAS ##
-
-def jugar():
-    print("Jugate lo que quieras pa, te lo ganaste")
-
-def paseo():
-    print("Te ganaste un paseo (si es de noche guardatelo para otro dia)")
-
-def album():
-    print("Estas de suerte, te ganaste escuchar un album de principio a fin, disfrutalo putita")
-
-def baño_musical():
-    print("Hoy te toca mover el orto mientras te bañás, te lo ganaste")
-
-## RECOMPENSAS TOCHAS ##
-
-
-
-
-
-## RANDOMIZADO CHILL ##
-
-def chill_random(probabilidades): ### DECLARAMOS FUNCION
-    r = random.random() ### RANDOMIZAMOS VALOR
-    for opciones, prob in probabilidades.items(): ### ARRANCAMOS CICLO FOR: OPC ES LA CLAVE DEL DICT, PROB EL VALOR, Y PROB.ITEMS() EL NUMERO MAXIMO DE VUELTAS (en este caso, seria el total de items)
-        if r < prob: ### SI LA PROB > R
-            return opciones ### SALE DE LA FUNCION DEVOLVIENDO LA CLAVE EN LA QUE CORTÓ
-        r -= prob ### SI NO RESTA EL VALOR Y VUELVE A ARRANCAR
-
-### EJEMPLO (MOMENTO EN EL QUE LO CREO)
-
-### SI R = 24, ENTRA AL BUCLE, Y ME DEVUELVE 1
-### SI R = 26, ENTRA AL BUCLE, RESTA PROB PORQUE NO ENTRÓ, DA LA SEGUNDA VUELVA Y DEVUELVE 2 PORQUE SI ENTRÓ
-
-## RANDOMIZADO CHILL ##
-
-
-
-
-## RANDOMIZADO TOCHO ##
-
-def tocho_random(probabilidades): ### DECLARAMOS FUNCION
-    r = random.random() ### RANDOMIZAMOS VALOR
-    for opciones, prob in probabilidades.items(): ### ARRANCAMOS CICLO FOR: OPC ES LA CLAVE DEL DICT, PROB EL VALOR, Y PROB.ITEMS() EL NUMERO MAXIMO DE VUELTAS (en este caso, seria el total de items)
-        if r < prob: ### SI LA PROB > R
-            return opciones ### SALE DE LA FUNCION DEVOLVIENDO LA CLAVE EN LA QUE CORTÓ
-        r -= prob ### SI NO RESTA EL VALOR Y VUELVE A ARRANCAR
-
-## RANDOMIZADO TOCHO ##
-
-### DEVOLUCIONES ###
-
-def devolver_chill():
-    print("Felicidades por terminar tu sesión, acá está tu recompensa: ")
-    premio = chill_random(opciones_chill)
-    if int(premio) == 1:
-        nada()
-    elif int(premio) == 2:
-        meditar()
-    elif int(premio) == 3:
-        musica()
-    elif int(premio) == 4:
-        comer()
-    elif int(premio) == 5:
-        mona()
-    elif int(premio) == 6:
-        sol()
-    elif int(premio) == 7:
-        baño()
-
-def devolver_tocho():
-    print("Felicidades por terminar tu dia de trabajo, acá está tu recompensa: ")
-    premio = tocho_random(opciones_tocho)
-    if int(premio) == 1:
-        jugar()
-    if int(premio) == 2:
-        paseo()
-    if int(premio) == 3:
-        album()
-    if int(premio) == 4:
-        baño_musical()
-
-### DEVOLUCIONES ###
-
-### CODIGO MAIN ###
-
-print("BIENVENIDO A LA RULETA DE LA VICTORIA")
-print("Usted esta aquí para elegir su recompensa")
-print("En primer lugar, qué tipo de recompensa quiere?")
-recompensa = input("Pulse 1 si es entre sesiones de trabajo o 2 si ya terminó de trabajar: ")
-
-if int(recompensa) == 1:
-    devolver_chill()
-elif int(recompensa) == 2:
-    devolver_tocho()
